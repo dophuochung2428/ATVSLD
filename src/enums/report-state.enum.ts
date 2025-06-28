42b62b5404325a79d83af2b2e4387d3fe15ef777
dophuochung2428 <dophuochung2428@gmail.com>
2025-06-28 09:28:42 +0700

    upcode gitlab

--- src/enums/report-state.enum.ts
+++ src/enums/report-state.enum.ts
@@ -2,12 +2,16 @@ export enum ReportState {
   Pending = 'PENDING',
   Typing = 'TYPING',
   Completed = 'COMPLETED',
-  Expired = 'EXPIRED'
+  Expired = 'EXPIRED',
+  WaitingForApproval = 'WAITING_FOR_APPROVAL',
+  Approved = 'APPROVED',
 }
 
 export const ReportStateLabel = {
   [ReportState.Pending]: 'Chờ báo cáo',
   [ReportState.Typing]: 'Nhập liệu',
   [ReportState.Completed]: 'Hoàn thành',
-  [ReportState.Expired]: 'Hết hạn'
+  [ReportState.Expired]: 'Hết hạn',
+  [ReportState.WaitingForApproval]: 'Chờ duyệt',
+  [ReportState.Approved]: 'Đã duyệt',
 };
