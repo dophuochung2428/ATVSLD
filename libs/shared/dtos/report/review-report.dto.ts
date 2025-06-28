ab855f7ad80e7f9a86fe635af16d3860072b12f3
dophuochung2428 <dophuochung2428@gmail.com>
2025-06-28 14:07:24 +0700

    update approval report2

--- libs/shared/dtos/report/review-report.dto.ts
+++ libs/shared/dtos/report/review-report.dto.ts
@@ -1,10 +1,23 @@
-import { IsEnum, IsOptional, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsNotEmpty, IsOptional, IsString, ValidateIf } from "class-validator";
+import { ReportState } from "src/enums/report-state.enum";
 
 export class ReviewReportDto {
-  @IsEnum(['APPROVED', 'REJECTED'])
-  status: 'APPROVED' | 'REJECTED';
+  @ApiProperty({
+    enum: ReportState,
+    description: 'Trạng thái duyệt báo cáo: APPROVED (chấp nhận) hoặc REJECTED (từ chối)',
+    example: ReportState.Approved,
+  }) 
+   @IsEnum(ReportState)
+  status: ReportState;
 
-  @IsOptional()
+    @ApiProperty({
+    description: 'Lý do từ chối báo cáo. Bắt buộc nếu status là REJECTED.',
+    required: false,
+    example: 'Nội dung báo cáo chưa đầy đủ',
+  })
+  @ValidateIf(o => o.status === ReportState.Rejected)
+  @IsNotEmpty({ message: 'Vui lòng nhập lý do từ chối nếu không chấp nhận báo cáo' })
   @IsString()
   reason?: string; // Lý do từ chối, bắt buộc nếu REJECTED
 }
